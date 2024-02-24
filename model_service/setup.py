ef9d0f1febb4765f7db3d6c55c5fd2192886fddf
Ariel Arévalo Alvarado <71473451+arielarevalo@users.noreply.github.com>
2024-02-23 20:50:03 -0600

    Initial commit

---
+++ model_service/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='model_service',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Ariel Arevalo Alvarado',
+    author_email='arielarevaloalvarado@gmail.com',
+    description=''
+)
